81334ba1f134afb3d271f61bc6e852b31a2ef653
Stan Seibert <sseibert@anaconda.com>
2021-02-15 14:25:00 -0600

    add comments to algorithm

--- metagraph_numba/compiler.py
+++ metagraph_numba/compiler.py
@@ -1,3 +1,35 @@
+"""
+Numba compiler plugin:
+
+This compiler plugin assumes that concrete algorithm implementations are
+"Numba-ready".  Specifically:
+
+* The decorated Python function is compilable by Numba.
+* The input and output data types can be boxed/unboxed by Numba automatically.
+
+To created a fused multiple tasks into a subgraph, all of the individual task
+functions are wrapped in the Numba @jit decorator with inlining turned on. A
+wrapper function that calls the tasks in the correct order is generated. Task
+function arguments are sorted into one of three categories using the
+SymbolTable class:
+
+* Inputs passed from tasks outside the subgraph ("var0", "var1", etc)
+* Values known at compile time ("const0", "const1", etc)
+* Results from other tasks in the subgraph ("ret0", "ret1", etc)
+
+The subgraph is walked to build the symbol table, then the wrapper function is
+generated as text (since Python doesn't have a better way to do this).
+Finally, the function is materialized by using exec() and populating the
+globals with the constants and functions that the wrapper references.  Numba
+will capture these global values when it finally compiles the function.
+
+Note that while the fused function is generated ahead of time, the actual
+Numba compilation is not triggered until the fused task is executed by Dask at
+some point in the future.  This is an implementation convenience, as
+generating a full Numba type signature to do ahead-of-time compilation is a
+big pain.
+"""
+
 import numba
 from metagraph.core.plugin import Compiler, CompileError
 from typing import Dict, List, Tuple, Callable, Hashable, Any
@@ -7,6 +39,34 @@ from dataclasses import dataclass, field
 
 @dataclass
 class SymbolTable:
+    """Container for mapping functions (identified by a dict key), Python
+    arguments, and input parameters (also identified by a dict key) to valid
+    Python identifiers.
+
+    This class is needed because Dask keys only need to be hashable, but are
+    not necessarily valid Python identifiers.  Also, Dask tasks reference
+    values and other tasks, which need to be mapped to identifiers as well.
+
+
+    The naming convention for attributes is as follows:
+      - `sym`: A symbol name that is a valid Python identifier.  This class
+        uses names like `constN`, `varN`, `funcN`, and `retN`.
+      - `key`: A key for a task in a Dask graph
+      - `func`: The callable function associated with a task
+      - `ret`: The return value from a function
+      - `var`: An external, unknown value, typically coming from Dask tasks
+        defined outside of the subgraph being considered.
+      - `const`: A Python value known at compile time that can be treated as a
+        constant.
+
+    The mappings in this class typically populated by calling register_var()
+    and register_func().  See docstrings below for more details.
+
+    Note that automatically generated symbol names are only guaranteed to be
+    unique for this instance of the symbol table.  Other symbol tables will
+    collide with this one.
+    """
+
     var_sym_to_key: Dict[str, Hashable] = field(default_factory=dict)
     var_key_to_sym: Dict[Hashable, str] = field(default_factory=dict)
     const_sym_to_value: Dict[str, Any] = field(default_factory=dict)
@@ -40,17 +100,33 @@ class SymbolTable:
         return f"{prefix}{value}"
 
     def register_var(self, key):
+        """Register an external values associated with `key`.
+        
+        Returns uniquely generated symbol name.
+        """
         var_sym = self.next_var()
         self.var_sym_to_key[var_sym] = key
         self.var_key_to_sym[key] = var_sym
         return var_sym
 
     def register_const(self, value):
+        """Register a Python object as a compile time constant.
+
+        Returns uniquely generated symbol name.
+        """
         const_sym = self.next_const()
         self.const_sym_to_value[const_sym] = value
         return const_sym
 
     def find_symbol(self, arg):
+        """Find the symbol associated with a given function argument.
+
+        If the argument is a known key for either an external value
+        or a previously registered function call, the appropriate symbol
+        (either `varN` or `retN`) for the value will be returned.
+
+        If this argument is not a known key, return None
+        """
         if ishashable(arg):
             # is this a Dask key that maps to an input variable?
             sym = self.var_key_to_sym.get(arg, None)
@@ -66,7 +142,18 @@ class SymbolTable:
         # return None means no symbol found
 
     def register_func(self, key, func, args):
-        """'Must register functions in toposort order"""
+        """Register a function call with return value associated with key and
+        the given argument list.
+
+        The function symbol and return value symbol will be generated and
+        returned. Arguments will be scanned and logged as `var` or `ret`
+        symbols (if they are known Dask keys), or the argument value will be
+        stored and a newly generated const symbol recorded in the argument
+        list.
+
+        Note that function calls must be registered in topologically sorted
+        dependency order, or the key discovery will not work.
+        """
         func_sym = self.next_func()
         ret_sym = self.next_ret()
         self.func_sym_to_key[func_sym] = key
@@ -93,6 +180,34 @@ def construct_call_wrapper_text(
     execute_keys: List[Hashable],
     output_key: Hashable,
 ) -> Tuple[str, Dict[str, Any]]:
+    """Create the Python source code of a wrapper function for a subgraph.
+
+    The function will be generated with the name `wrapper_name and take as
+    input arguments the values associated with `input_keys` in the order
+    given.  Functions will be executed in the order provided by
+    `execute_keys`, and the function will return the result associated with
+    `output_key`.  All tasks and inputs need to be recorded in the provided
+    `symbol_table` before calling this function.  
+
+    Because constants and function definitions cannot be serialized into the
+    wrapper source in general, the wrapper will also indicate all of those
+    symbols are globals using the `global` statement.  When using exec() to 
+    parse the wrapper source, the const and func values will need to be
+    put into the globals dict passed to exec().
+
+    The return value of this function will look something like:
+
+        def subgraph0(var0, var1):
+            global const0
+            global const1
+            global func0
+            global func1
+
+            ret0 = func0(var0, var1, const0)
+            ret1 = func1(ret0, const1)
+
+            return ret1
+    """
 
     wrapper_globals = symbol_table.const_sym_to_value.copy()
     wrapper_globals.update(symbol_table.func_sym_to_func)
@@ -129,25 +244,41 @@ def construct_call_wrapper_text(
 
 def compile_wrapper(
     wrapper_name: str, wrapper_text: str, wrapper_globals: Dict[str, Any]
-):
+) -> Callable:
+    """Compile the source code for a wrapper function into a Python function
+    and return it.
+
+    Any globals expected by the function need to be passed in the
+    wrapper_globals dict.
+    """
     my_globals = wrapper_globals.copy()
     exec(wrapper_text, my_globals)
     return my_globals[wrapper_name]
 
 
 class NumbaCompiler(Compiler):
-    """This compiler compiles functions using Numba.
-    """
-
     def __init__(self, name="numba"):
         super().__init__(name=name)
         self._subgraph_count = 0
 
     def compile_algorithm(self, algo, literals):
+        """Wrap a single function for JIT compilation and execution.
+        
+        literals is not used for anything currently
+        """
         return numba.jit(algo.func)
 
-    def compile_subgraph(self, subgraph: Dict, inputs: List[str], output: str):
+    def compile_subgraph(
+        self, subgraph: Dict, inputs: List[Hashable], output: Hashable
+    ) -> Callable:
+        """Fuse a subgraph of tasks into a single compiled function.
+
+        It is assumed that the function will be called with values corresponding to
+        `inputs` in the order they are given.
+        """
         tbl = SymbolTable()
+
+        # must populate the symbol table in toposort order
         toposort_keys = list(toposort(subgraph))
 
         # register the inputs as variables
@@ -164,12 +295,15 @@ class NumbaCompiler(Compiler):
                 kwargs = kwargs[0](kwargs[1])
             if len(kwargs) != 0:
                 raise CompileError(
-                    "NumbaCompiler only supports functions bound kwargs.\n"
-                    f"When compiling:\n{delayed_algo.func_label}\nfound kwargs:\n{kwargs}"
+                    "NumbaCompiler only supports functions with bound kwargs.\n"
+                    f"When compiling:\n{delayed_algo.func_label}\nfound unbound kwargs:\n{kwargs}"
                 )
+            # for maximum optimization, inline every task function during
+            # compilation of the wrapper
             jit_func = numba.jit(inline="always")(delayed_algo.algo.func)
             tbl.register_func(key, jit_func, args)
 
+        # generate the wrapper
         subgraph_wrapper_name = "subgraph" + str(self._subgraph_count)
         self._subgraph_count += 1
         wrapper_text, wrapper_globals = construct_call_wrapper_text(
